813cb65abcc03f4147d74bf9548353c7f1d7c76f
Ruben Reyes <ruben.burgosr@uanl.edu.mx>
2023-05-05 15:46:38 -0600

    Clase main

--- Main.java
+++ Main.java
@@ -1,26 +1,28 @@
+import java.sql.ResultSet;
+
+import workers.Piloto;
+
 public class Main {
   public static void main(String[] args) {
     DataBase db = DataBase.getConnection();
 
-    String nombre, nacionalidad, edad, victorias, salario, numEmpleado, numPiloto, departamento, seccion, area;
+    String nombre, nacionalidad, numEmpleado, departamento, seccion, area;
+    int edad, numPiloto, victorias;
+    float salario;
 
-    // TODO hacer el menu para seleccionar que se desea hacer (crear, leer,
-    // actualizar, borrar)
-
-    // Cada dato se asignaría usando la función reqString de la clase InputUtils
     nombre = InputUtils.reqString("Nombre: ");
-    edad = InputUtils.reqString("Edad: ");
-    salario = InputUtils.reqString("Salario: ");
+    edad = InputUtils.reqInt("Edad: ");
+    salario = InputUtils.reqFloat("Salario: ");
     numEmpleado = InputUtils.reqString("# de empleado: ");
-    numPiloto = InputUtils.reqString("# de piloto: ");
-    victorias = InputUtils.reqString("# de victorias: ");
+    numPiloto = InputUtils.reqInt("# de piloto: ");
+    victorias = InputUtils.reqInt("# de victorias: ");
     nacionalidad = InputUtils.reqString("Nacionalidad: ");
 
-    // Si selecciona Crear => Crear Piloto. Se crearía de la siguiente forma
     Piloto piloto = new Piloto(nombre, edad, salario, numEmpleado, numPiloto, victorias, nacionalidad);
 
     try {
-      db.create("pilotos", piloto.getQueryValues());
+      db.create("pilotos", piloto.getQueryValues()); // CREAR piloto
+      db.findOne("pilotos", numEmpleado); // BUSCAR piloto por numEmpleado
     } catch (ErrorOperation e) {
       System.out.println(e);
     }
