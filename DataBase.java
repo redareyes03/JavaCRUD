5c74a04bea833043f089163bb019a67046fcfca4
Ruben Reyes <ruben.burgosr@uanl.edu.mx>
2023-05-05 15:46:10 -0600

    Se agrego método findOne

--- DataBase.java
+++ DataBase.java
@@ -5,7 +5,7 @@ public class DataBase {
   private static DataBase instance = null;
   private String url = "jdbc:postgresql://localhost:5432/Formula1";
   private String user = "postgres";
-  private String password = "pass";
+  private String password = "337799";
   private Connection connection = null;
 
   private DataBase() {
@@ -33,6 +33,17 @@ public class DataBase {
     }
   }
 
+  public ResultSet findOne(String dbName, String numEmpleado) throws ErrorOperation {
+    String query = "select * from " + dbName + " where numEmpleado = " + numEmpleado;
+    ResultSet data = null;
+    try {
+      PreparedStatement statement = connection.prepareStatement(query);
+      data = statement.executeQuery();
+    } catch (Exception e) {
+      throw new ErrorOperation("Error al buscar el registro con el pk " + numEmpleado);
+    }
+    return data;
+  }
 }
 
 class ErrorOperation extends Exception {
